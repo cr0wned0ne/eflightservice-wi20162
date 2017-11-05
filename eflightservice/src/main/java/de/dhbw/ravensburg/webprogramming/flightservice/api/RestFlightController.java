da120e41eda7be6ecab751cc8ee210dc3b0a2644
Stefan Elmlinger <stefan.elmlinger@outlook.com>
2017-11-05 23:02:18 +0100

    Created Javadoc and completed Rest endpoint to save flights with all parameters

--- eflightservice/src/main/java/de/dhbw/ravensburg/webprogramming/flightservice/api/RestFlightController.java
+++ eflightservice/src/main/java/de/dhbw/ravensburg/webprogramming/flightservice/api/RestFlightController.java
@@ -30,12 +30,23 @@ public class RestFlightController {
 	 * @param flug The flight to save / Update
 	 */
 	@RequestMapping(path = "api/flight", method = RequestMethod.PUT)
-	public String putFlight(@RequestParam String flightId, @RequestParam String destination, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm") Date startDate){
+	public String putFlight(@RequestParam String flightId, 
+			@RequestParam String destination, 
+			@RequestParam String origin, 
+			@RequestParam String destinationGate,
+			@RequestParam String originGate,
+			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm") Date startDate,
+			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm") Date endDate){
 		System.out.println(flightId);
 		FlightEntity flightEntity = new FlightEntity();
 		flightEntity.setFlightId(flightId);
 		flightEntity.setDestination(destination);
+		flightEntity.setOrigin(origin);
+		flightEntity.setDestinationGate(destinationGate);
+		flightEntity.setOriginGate(originGate);
 		flightEntity.setStartDate(startDate);
+		flightEntity.setEndDate(endDate);
+
 		flightRepo.save(flightEntity);
 		return "OK";
 	}
